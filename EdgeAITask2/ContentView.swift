76d6d47a84f8e586cf8dd07a95b6cce3683cb544
Siddharthsinh Rathod <131669187+siddharthsinh-dev@users.noreply.github.com>
2026-03-04 12:50:14 +0530

    Initial Commit

---
+++ EdgeAITask2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EdgeAITask2
+//
+//  Created by Siddharthsinh Rathod on 04/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EdgeAITask2/EdgeAITask2App.swift
@@ -0,0 +1,17 @@
+//
+//  EdgeAITask2App.swift
+//  EdgeAITask2
+//
+//  Created by Siddharthsinh Rathod on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct EdgeAITask2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
